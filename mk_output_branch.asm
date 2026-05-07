341bf260565b35386219740cfc2c27f45495e98b
Micah Krosby <micahkrosby@gmail.com>
2026-05-07 10:29:55 -0700

    Properly document mk_output_branch

--- mk_output_branch.asm
+++ mk_output_branch.asm
@@ -1,10 +1,28 @@
-// Wrapper to dynamically select the proper output based on number input.
-// Input should be according to ASCII codes.
-// Written by Micah Krosby
-
+//*****************************************************************************************************
+//
+// mk_output_branch : Branch between ge_output_X functions using a switch statement
+//_____________________________________________________________________________________________________
+//
+// Inputs           : ge_dynamic_x -> ASCII symbol code to decode
+// Outputs          : none... Instead, outputs to screen
+// Preconditions    : Must set column to print to... using ge_currentColumn
+//_____________________________________________________________________________________________________
+//
+// Pseudocode       : "n" is a number between 0 and 9.
+//
+// ge_output_return = mk_output_branch_return; // Make sure that return is set for ge_output
+// if (ge_dynamic_x == 45) ge_output_-();
+// if (ge_dynamic_x == 43) ge_output_+();
+// switch (ge_dynamic_x - 48) {
+// case "n":
+//   ge_output_"n"();
+// }
+// return;
+//
+//*****************************************************************************************************
 (mk_output_branch)
 //Exit directly from ge_output_x if possible
-@ge_output_branch_return
+@mk_output_branch_return
 D=M
 @ge_output_return
 M=D
@@ -51,4 +69,5 @@ D-1;JEQ
 
 //Exit if no match
 @mk_output_branch_return
+A=M
 0;JMP
